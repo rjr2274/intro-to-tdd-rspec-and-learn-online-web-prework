99bd99ba4074e1e80530173a773d54372408da06
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-06-01 06:55:05 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,4 +1,4 @@
-require_relative ./current_age_for_birth_year.#!/usr/bin/env ruby -wKU
+require_relative './current_age_for_birth_year.'#!/usr/bin/env ruby -wKU
 
 puts "What year were you born?"
 birth_year = get.to_i
